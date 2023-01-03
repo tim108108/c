dca5eb9e7ac0cf5b7ececa7c613947aca741aa30
tim108108 <tim108108@gmail.com>
2023-01-03 18:28:01 +0800

    2

--- test.c
+++ test.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+    int data;
+    struct node *next;
+}Node;
 
 int main(){
-printf("hi\n");
-return 0;
+    printf("hi a1\n");
+    Node a;
+    a.data=12;
+    printf("a is %d(ptr=%p).\n",a.data,a.next);
+    printf("memory is location of a is %p\n",&a);
 }
 
