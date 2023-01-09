821ef249fe461148942e519726fbdbe41f258ed5
tim108108 <tim108108@gmail.com>
2023-01-09 11:24:53 +0800

    Q1

--- test.c
+++ test.c
@@ -6,11 +6,47 @@ typedef struct node{
     struct node *next;
 }Node;
 
-int main(){
-    printf("hi a1\n");
+void linklist(){
     Node a;
     a.data=12;
     printf("a is %d(ptr=%p).\n",a.data,a.next);
     printf("memory is location of a is %p\n",&a);
+}
+void Q1(int *in, int size){
+    int *ptr = in;
+    for(int i=0;i<size;i++){
+        if (*ptr == i){
+            ptr++;
+        }
+        else{
+            printf("%d\n",i);
+
+        }
+    }
+    
+}
+
+void Q2(int *a,int size,int b){
+    int *ptr=a;
+    
+    while(*ptr<b*100){
+        ptr++;
+    }
+    
+    for(int i=0;i<(b+1)*100;i++){
+        if(*ptr==i){
+            ptr++;
+        }
+        else{
+            printf("%d\n",i);
+        }
+    }
+}
+int main(){
+    printf("This is c test\n");
+    //linklist();
+    int a[]={1,3,5,7,9};
+    //Q1(&a[0],10);
+    Q2(&a[0],200,0);
 }
 
